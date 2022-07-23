f8a8c26a2be4d79d8f08bf0b73d4396dca328d52
Cakir <ahmetcaakirr@gmail.com>
2022-07-23 16:42:43 +0300

    detail/help page is confirmed

--- lib/view-model/braille_page01.dart
+++ lib/view-model/braille_page01.dart
@@ -2,6 +2,7 @@ import 'package:braille_alfabesi/model/classes.dart';
 import 'package:braille_alfabesi/model/lists.dart';
 import 'package:braille_alfabesi/model/variables.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class BraillePage01 extends StatefulWidget {
   const BraillePage01({Key? key}) : super(key: key);
@@ -12,6 +13,14 @@ class BraillePage01 extends StatefulWidget {
 
 class _BraillePage01State extends State<BraillePage01> {
   int imageIndex = 0;
+  final String _github = 'https://github.com/ahmetcakr';
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    GestureDetector().behavior;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -20,7 +29,51 @@ class _BraillePage01State extends State<BraillePage01> {
         title: const Text(page01AppBarTitle),
         actions: [
           IconButton(
-              onPressed: () {}, icon: const Icon(Icons.question_mark_rounded))
+              onPressed: () {
+                showDialog(
+                    context: context,
+                    builder: (ctx) => AlertDialog(
+                          actions: [
+                            TextButton(
+                                onPressed: () {
+                                  launch(_github);
+                                },
+                                child: const Text('Github')),
+                            ElevatedButton(
+                              child: const Text('OK'),
+                              onPressed: () {
+                                Navigator.of(ctx).pop();
+                              },
+                            ),
+                          ],
+                          title: SizedBox(
+                              height: 100,
+                              width: 100,
+                              child: Image.asset('assets/cakirlittle.png')),
+                          content: Card(
+                            elevation: 5,
+                            child: SizedBox(
+                              height: 250,
+                              width: 250,
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                children: const [
+                                  Text('Braille Alfabesi Nedir ?',
+                                      textAlign: TextAlign.center),
+                                  Text('\n\n', textAlign: TextAlign.center),
+                                  Text(
+                                      'Braille Alfabesi, Braille alfabesi veya Körler alfabesi;\n\n'
+                                      " 1821 yılında Louis Braille tarafından geliştirilmiş görme engelli insanların okuyup yazması için kullanılan bir alfabe yöntemidir.\n"
+                                      " İki kolon taşıyan dikdörtgen düzen üzerine dizilmiş altı kabartılmış noktadan oluşur. \n"
+                                      "Her iki kolonda üçer nokta bulunur. Noktalardan her biri altmışdört farklı kombinasyondan birini oluşturması için farklı şekillerde dizilir. ",
+                                      textAlign: TextAlign.center),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ));
+              },
+              icon: const Icon(Icons.question_mark_rounded))
         ],
       ),
       body: SingleChildScrollView(
